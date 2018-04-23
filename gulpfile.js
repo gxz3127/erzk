b47a15d78e035e868d01d26d836be620694790cd
guomaomi <982548809@qq.com>
2018-04-23 10:14:06 +0800

    渲染数据

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,17 @@
 var gulp=require("gulp");
 var server=require("gulp-webserver");
+var data=require("./src/data/data.json");
 gulp.task("default",function(){
     gulp.src("src")
     .pipe(server({
-        port:"7080",
+        port:"9999",
         open:true,
-        host:"localhost",
+        host:"169.254.242.144",
         livereload:true,
         middleware:function(req,res,next){
-            if()
+            if(/\/list/g.test(req.url)){
+                res.end(JSON.stringify(data))
+            }  
             next();
         }
     }))
